8a7b54ff29bf5373a0e5669611c1ae2cd767c1aa
zhoutengteng <zhoutengteng@tbkt.cn>
2018-08-02 18:30:00 +0800

    init 初始化

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <transition name="fade">
+      <P>欢迎使用Znote</P>
+    </transition>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss">
+  @import "~SCSS/index";
+</style>
+<style rel="stylesheet/scss" lang="scss" scoped>
+  #app{
+    @include center;
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    p{
+      font-size: 40px;
+      color: #333333;
+    }
+    .fade-enter-active, .fade-leave-active {
+      transition: all 3s ease;
+    }
+    .fade-enter, .fade-leave-to{
+      opacity: 0;
+    }
+  }
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,34 @@
+/**
+* Created by Administrator on 2018/8/2.
+*/
+<template>
+    <div id="root">
+
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    import {URL, api} from 'API';
+
+    export default {
+        name: '',
+        components: {},
+        created(){
+
+        },
+        props: {},
+        data(){
+            return {
+                apiData: {}
+            }
+        },
+        computed: {},
+        methods: {}
+    };
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+
+
+</style>
+
